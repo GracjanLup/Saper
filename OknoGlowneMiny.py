7397f135d94b92391355c3d0a98af741421ce55a
GracjanLup <gracjan.popiolkowski@gmail.com>
2022-06-04 15:51:54 +0200

    Prawy_przycisk v0.1
    Dodałem frame panel_gorny dla Emmy.
    Plus przygotowuje się już do pracy nad prawym przyciskiem - oflagowania.

--- OknoGlowneMiny.py
+++ OknoGlowneMiny.py
@@ -4,17 +4,27 @@ from tkinter import messagebox
 import tkinter.font as font
 import time
 import random
+from PIL import Image, ImageTk, ImageFilter
+
+# Pamiętajcie o:
+# "pip install Pillow" w cmd'ku
 
 def Start():
     print("siema")
-    PanelGlowny(root)
+    Panel_glowny(root)
 
-def PanelGlowny(root):
-    pola_gry = [tk.Button(frame, width = 2, height = 1) for i in range(Kolumny*Wiersze)]
+def Panel_glowny(root):
+    pola_gry = [tk.Button(frame, width = 30, height = 30, image=test) for i in range(Kolumny*Wiersze)]
     for i in range(Wiersze):
         for j in range(Kolumny):
             pola_gry[i*Kolumny + j].grid(row = i+1, column = j)
     return pola_gry
+
+def Prawy_przycisk():
+    pola_gry[x,y].config(image="Grafika/flaga.png")
+
+def Lewy_przycisk():
+    print("Ok")
 
 def Wylosowanie_min():
     liczba_min = 20
@@ -31,13 +41,21 @@ Wiersze = 10
 
 root = tk.Tk()
 root.title('Saper')
-root.geometry('600x400')
+root.geometry('350x400')
+
+obraz = Image.open("Grafiki/test.png")
+obraz = obraz.resize((30, 30))
+obraz = obraz.filter(ImageFilter.CONTOUR)
+test = ImageTk.PhotoImage(obraz)
 
 InfoFont = font.Font(family='Tahoma', size=20)
 ButtonFont = font.Font(family='Tahoma', size=20)
 
+panel_gorny = tk.Frame(root, bg="pink")
+panel_gorny.place(relwidth=1, relheight=0.125)
+
 frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.4, relheight=0.66, relx=0.3, rely=0.17)
+frame.place(relwidth=1, relheight=0.875, rely=0.125)
 
 StartButton = Button(root, text="Start", padx=10, pady=5, font=ButtonFont, command=lambda:[StartButton.pack_forget(), Start()])
 StartButton.pack()
