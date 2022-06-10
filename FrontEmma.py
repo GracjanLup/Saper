54dbd73cd51b2b6d8d8bdf11afaceff2d5fccfbe
emmamaciejewska <105227547+emmamaciejewska@users.noreply.github.com>
2022-06-10 17:04:09 +0200

    Update FrontEmma.py
    dodałam przycisk buźki

--- FrontEmma.py
+++ FrontEmma.py
@@ -1,9 +1,10 @@
+
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
 import tkinter.font as font
 import time
-
+from PIL import Image, ImageTk
 CZAS=0
 
 def Start():
@@ -20,14 +21,18 @@ def PanelGlowny(root):
     return pola_gry
 
 def PanelGorny(root):
-    zegar= tk.Label(bg="black", fg="red", font=("Digital-7", 20))
-    zegar.place(x=370, y=10)
-    zegar["text"]="0001"
+
     aktualizacjaZegara(root, zegar)
 
-    panel_gorny=[zegar]
+    # licznik_min=tk.Label(root, bg="black", fg="red", font=("Digital-7", 20))
+    # licznik_min.place(x=200, y=10)
+    # licznik_min["text"]="000"
+    # aktualizacjaLicznikaMin(root, licznik_min)
+
+
+
+    panel_gorny=[zegar, emotikona]
     return panel_gorny
-#w tej funckji będzie jeszcze licznik min i przycisk postaci buźki
 
 def aktualizacjaZegara(root, zegar):
     global CZAS
@@ -35,6 +40,7 @@ def aktualizacjaZegara(root, zegar):
     zegar["text"]="0"*(3-len(str(CZAS)))+str(CZAS)
     root.after(1000, aktualizacjaZegara, root, zegar)
 
+# def aktualizacjaLicznikaMin()
 Kolumny = 10
 Wiersze = 10
 root = tk.Tk()
@@ -46,10 +52,19 @@ panel_gorny = tk.Frame(root, bg="pink")
 panel_gorny.place(relwidth=1, relheight=0.125)
 
 frame = tk.Frame(root, bg="white")
-frame.place(relwidth=0.4, relheight=0.66, relx=0.3, rely=0.17)
 frame.place(relwidth=1, relheight=0.875, rely=0.125)
 
-StartButton = Button(root, text="Start", padx=10, pady=5, font=ButtonFont, command=lambda:[StartButton.pack_forget(), Start()])
-StartButton.pack()
+zegar= tk.Label(bg="black", fg="red", font=("Digital-7", 20))
+zegar.place(x=370, y=10)
+zegar["text"]="0001"
+
+emotikona_przycisk=Image.open("buzka_usmiech.png")
+emotikona_przycisk=emotikona_przycisk.resize((60,60))
+emotikona_przycisk=ImageTk.PhotoImage(emotikona_przycisk)
+emotikona=tk.Button(root, width=20, height=20, image=emotikona_przycisk)
+emotikona.place(x=290, y=10)
+
+
+Start()
 panel_gorny=PanelGorny(root)
 root.mainloop()
