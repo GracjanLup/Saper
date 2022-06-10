fff236c386ed933247279909797fdee52b7ed53c
GracjanLup <gracjan.popiolkowski@gmail.com>
2022-06-10 23:25:02 +0200

    Grafiki, funkcja end i prawego przycisku myszy
    Zinicjalizowałem i wstawiłem grafiki cyfr,
    Funkcja końca gry,
    Funkcja prawego przycisku,
    Funkcja ponownego prawego przycisku.

--- OknoGlowneMiny.py
+++ OknoGlowneMiny.py
@@ -60,6 +60,24 @@ def run_game(number_of_mines, number_of_rows, number_of_columns):
     for i in range(number_of_rows):
         for j in range(number_of_columns):
             pola_gry[i * number_of_columns + j].grid(row = j, column = i)
+    # for j in range(number_of_columns*number_of_rows):
+    #     pola_gry[j].bind('<Button-3>', lambda event: right_click(j))
+    #     pola_gry[j].focus_set()
+    pola_gry[0].bind('<Button-3>', lambda event: right_click(0))
+    pola_gry[1].bind('<Button-3>', lambda event: right_click(1))
+    pola_gry[2].bind('<Button-3>', lambda event: right_click(2))
+    pola_gry[3].bind('<Button-3>', lambda event: right_click(3))
+    pola_gry[4].bind('<Button-3>', lambda event: right_click(4))
+    pola_gry[5].bind('<Button-3>', lambda event: right_click(5))
+    pola_gry[6].bind('<Button-3>', lambda event: right_click(6))
+    pola_gry[7].bind('<Button-3>', lambda event: right_click(7))
+    pola_gry[8].bind('<Button-3>', lambda event: right_click(8))
+    pola_gry[9].bind('<Button-3>', lambda event: right_click(9))
+    pola_gry[10].bind('<Button-3>', lambda event: right_click(10))
+    pola_gry[11].bind('<Button-3>', lambda event: right_click(11))
+    pola_gry[12].bind('<Button-3>', lambda event: right_click(12))
+    pola_gry[13].bind('<Button-3>', lambda event: right_click(13))
+    pola_gry[14].bind('<Button-3>', lambda event: right_click(14))
 
 # sprawdzenie pozycji po kliknięciu - czy bomba czy wyświetlić liczbę
 # do dokończenia sprawdzanie sąsiadów w przypadku, gdy kliknięty przycisk jest przy krawędzi
@@ -68,14 +86,26 @@ def check_position(i):
     count = 0
     print(i)
     if i in mines_positions:
-        pola_gry[i].config(image=mina)
+        pola_gry[i].config(image=mina_red)
+        end_game(i)
     else:
         numbers = [i - 9, i - 8, i - 7, i - 1, i + 1, i + 7, i + 8, i + 9]
-        print(i)
         for x in numbers:
             if x in mines_positions:
                 count += 1
-        pola_gry[i].config(image=cyfra1, text='')
+        # warunek tego ile jest bomb do okoła - wstawia odpowiednie zdjęcie
+        if count == 1:
+            pola_gry[i].config(image=cyfra1)
+        elif count == 2:
+            pola_gry[i].config(image=cyfra2)
+        elif count == 3:
+            pola_gry[i].config(image=cyfra3)
+        elif count == 4:
+            pola_gry[i].config(image=cyfra4)
+        elif count == 5:
+            pola_gry[i].config(image=cyfra5)
+        elif count == 0:
+            pola_gry[i].config(image=cyfra0)
     # do dokończenia sprawdzanie sąsiadów
 
 # generowanie pozycji min
@@ -94,39 +124,72 @@ def hide_all_widgets():
     for widgets in root.winfo_children():
         widgets.destroy()
 
+# funkcja końca gry
+def end_game(i):
+    print("Przegrałeś")
+    mines_positions.remove(i)
+    for i in range(len(pola_gry)):
+        pola_gry[i].config(command='')
+    for j in mines_positions:
+        pola_gry[j].config(image=mina)
+
+# funkcja prawego przycisku myszy
+def right_click(a):
+    # print(a)
+    pola_gry[a].config(image=flaga, command='')
+    pola_gry[a].bind('<Button-3>', lambda event: reset(a))
+
+# Ponowne wciesnięcie prawego przycisku myszy
+def reset(a):
+    pola_gry[a].config(image=test, command=partial(check_position, a))
+    pola_gry[a].bind('<Button-3>', lambda event: right_click(a))
+
 root = tk.Tk()
 root.title('Saper')
 root.geometry('1200x800')
 
 obraz = Image.open("Grafiki/test.png")
-obraz = obraz.resize((30, 30))
+obraz = obraz.resize((40,40))
 test = ImageTk.PhotoImage(obraz)
 
 # inicjalizacja grafiki dla bomby
 mina = Image.open("Grafiki/bomba.png")
-mina = mina.resize((30,30))
+mina = mina.resize((40,40))
 mina = ImageTk.PhotoImage(mina)
+
+mina_red = Image.open("Grafiki/bomba_red.png")
+mina_red = mina_red.resize((40,40))
+mina_red = ImageTk.PhotoImage(mina_red)
+
+# inicjalizacja grafiki dla flagi
+flaga = Image.open("Grafiki/flaga.png")
+flaga = flaga.resize((40,40))
+flaga = ImageTk.PhotoImage(flaga)
 
 # inicjalizacja grafik liczb
 cyfra1 = Image.open("Grafiki/1.png")
-cyfra1 = cyfra1.resize((30,30))
+cyfra1 = cyfra1.resize((40,40))
 cyfra1 = ImageTk.PhotoImage(cyfra1)
 
 cyfra2 = Image.open("Grafiki/2.png")
-cyfra2 = cyfra2.resize((30,30))
+cyfra2 = cyfra2.resize((40,40))
 cyfra2 = ImageTk.PhotoImage(cyfra2)
 
 cyfra3 = Image.open("Grafiki/3.png")
-cyfra3 = cyfra3.resize((30,30))
+cyfra3 = cyfra3.resize((40,40))
 cyfra3 = ImageTk.PhotoImage(cyfra3)
 
 cyfra4 = Image.open("Grafiki/4.png")
-cyfra4 = cyfra4.resize((30,30))
+cyfra4 = cyfra4.resize((40,40))
 cyfra4 = ImageTk.PhotoImage(cyfra4)
 
 cyfra5 = Image.open("Grafiki/5.png")
-cyfra5 = cyfra5.resize((30,30))
+cyfra5 = cyfra5.resize((40,40))
 cyfra5 = ImageTk.PhotoImage(cyfra5)
+
+cyfra0 = Image.open("Grafiki/0.png")
+cyfra0 = cyfra0.resize((40,40))
+cyfra0 = ImageTk.PhotoImage(cyfra0)
 
 info_font = font.Font(family='Tahoma', size=20)
 button_font = font.Font(family='Tahoma', size=20)
