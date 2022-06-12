5d0571ee33ca3b90a0bc1ca370bbbdc899936fc7
emmamaciejewska <105227547+emmamaciejewska@users.noreply.github.com>
2022-06-12 16:33:24 +0200

    Update FrontEmma.py

--- FrontEmma.py
+++ FrontEmma.py
@@ -1,4 +1,3 @@
-
 import tkinter as tk
 from tkinter import *
 from tkinter import messagebox
@@ -7,12 +6,16 @@ import time
 from PIL import Image, ImageTk, ImageFilter
 
 CZAS=0
-LM=0
+LICZBAMIN=10
+LICZBASZCZUROW=LICZBAMIN
 
 def Witaj():
-    print("siema")
-    emotikona.config(image=przycisk)
-    emotikona.after(400, aktualizujEmotke)
+    # print("siema")
+    emotikona.config(image=przyciskwow)
+    emotikona.after(300, aktualizujEmotke)
+
+def koniecgry_emotikona():
+    emotikona.config(image=przyciskprzegrany)
 
 def aktualizujEmotke():
     emotikona.config(image=emotikona_przycisk)
@@ -31,8 +34,8 @@ def PanelGlowny(root):
 
 def PanelGorny(root):
     aktualizacjaZegara(root, zegar)
-    aktualizacjaLicznikaMin(root, licznik_min)
-    panel_gorny=[zegar]
+    aktualizacjaLicznikaMin(licznik_min)
+    panel_gorny=[zegar, licznik_min]
     return panel_gorny
 
 def aktualizacjaZegara(root, zegar):
@@ -41,11 +44,22 @@ def aktualizacjaZegara(root, zegar):
     zegar["text"]="0"*(3-len(str(CZAS)))+str(CZAS)
     root.after(1000, aktualizacjaZegara, root, zegar)
 
-def aktualizacjaLicznikaMin(root, licznik_min):
-    global LM
-    for mina in range (LM, -1):
+def aktualizacjaLicznikaMin(licznik_min):
+    global LICZBAMIN
+    for mina in range (LICZBAMIN, -1):
         print(mina)
-    licznik_min["text"]="000"
+# dałabym tutaj odniesienie do funkcji oflagowania, gdzie jest wstępna liczba flag-- poziom łatwy, średni, trudny
+    licznik_min["text"]=LICZBAMIN
+
+def zmiana_licznika_min(licznik_min):
+    wartoscpoczatkowa=LICZBAMIN
+    wartoscpooflagowaniu=LICZBAMIN-1
+    licznik_min.config(wartośćpooflagowaniu)
+
+def right_click(a):
+    zmniejszanie_licznika_min=LICZBAMIN-1
+    pola_gry[a].config(image=szczur, command='')
+    pola_gry[a].bind('<Button-3>', lambda event: reset(a))
 
 
 Kolumny = 10
@@ -65,20 +79,29 @@ zegar = tk.Label(bg="black", fg="red", font=("Digital-7", 20))
 zegar.place(x=190, y=10)
 zegar["text"]="001"
 
+szczur = Image.open("szczur.png")
+szczur = szczur.resize((40,40))
+szczur = ImageTk.PhotoImage(szczur)
+
+licznik_min=tk.Label(root, bg="black", fg="red", font=("Digital-7", 20))
+licznik_min.place(x=0, y=10)
+licznik_min["text"]="001"
+pola_gry=tk.Button(root, command=zmiana_licznika_min)
+
 emotikona_przycisk=Image.open("buzka_usmiech.png")
 emotikona_przycisk=emotikona_przycisk.resize((40,40))
 emotikona_przycisk=ImageTk.PhotoImage(emotikona_przycisk)
 emotikona=tk.Button(root, width=40, height=40, image=emotikona_przycisk, command=lambda:[Witaj()])
 emotikona.place(x=97, y=0)
 
-licznik_min=tk.Label(root, bg="black", fg="red", font=("Digital-7", 20))
-licznik_min.place(x=0, y=10)
-licznik_min["text"]="000"
+przyciskwow = Image.open("buzka_wow.png")
+przyciskwow = przyciskwow.resize((30,30))
+przyciskwow = ImageTk.PhotoImage(przyciskwow)
 
-
-przycisk = Image.open("buzka_wow.png")
-przycisk = przycisk.resize((30,30))
-przycisk = ImageTk.PhotoImage(przycisk)
+przyciskprzegrany=Image.open("buzka_smierc.png")
+przyciskprzegrany=przyciskprzegrany.resize((30,30))
+przyciskprzegrany=ImageTk.PhotoImage(przyciskprzegrany)
+pola_gry = tk.Button(root, command=koniecgry_emotikona)
 
 Start()
 panel_gorny=PanelGorny(root)
