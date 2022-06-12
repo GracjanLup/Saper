16916a3510f3044bab38da2377e4e080b12c0ebb
GracjanLup <gracjan.popiolkowski@gmail.com>
2022-06-12 19:40:16 +0200

    Update OknoGlowneMiny.py

--- OknoGlowneMiny.py
+++ OknoGlowneMiny.py
@@ -51,12 +51,25 @@ def run_game(number_of_mines, number_of_rows, number_of_columns):
     mines_positions = draw_of_mines(number_of_mines, number_of_fields)
 
     # definiowanie wyglądu - panel górny
+
     upper_panel = tk.Frame(root, bg="grey")
     upper_panel.place(relwidth=1, relheight=0.125)
 
+    zegar = tk.Label(bg="black", fg="red", font=("Digital-7", 20))
+    zegar.place(x=190, y=10)
+    zegar["text"]="001"
+
+    licznik_min = tk.Label(root, bg="black", fg="red", font=("Digital-7", 20))
+    licznik_min.place(x=0, y=10)
+    licznik_min["text"]="001"
+
+    # pola_gry=tk.Button(root, command=zmiana_licznika_min)
+
+    # gorny_start()
+
     # definiowanie wyglądu - panel dolny
     main_panel = tk.Frame(root, bg="white")
-    main_panel.place(rely=0.5, relx=0.5, anchor=CENTER)
+    main_panel.place(relwidth=1, relheight=0.874, rely=0.125)
 
     # definicja zmiennej globalnej przechowującej pola gry
     global pola_gry
@@ -69,21 +82,74 @@ def run_game(number_of_mines, number_of_rows, number_of_columns):
         for j in range(number_of_columns):
             pola_gry[i * number_of_columns + j].grid(row = j, column = i)
 
-    pola_gry[0].bind('<Button-3>', lambda event: right_click(0))
-    pola_gry[1].bind('<Button-3>', lambda event: right_click(1))
-    pola_gry[2].bind('<Button-3>', lambda event: right_click(2))
-    pola_gry[3].bind('<Button-3>', lambda event: right_click(3))
-    pola_gry[4].bind('<Button-3>', lambda event: right_click(4))
-    pola_gry[5].bind('<Button-3>', lambda event: right_click(5))
-    pola_gry[6].bind('<Button-3>', lambda event: right_click(6))
-    pola_gry[7].bind('<Button-3>', lambda event: right_click(7))
-    pola_gry[8].bind('<Button-3>', lambda event: right_click(8))
-    pola_gry[9].bind('<Button-3>', lambda event: right_click(9))
-    pola_gry[10].bind('<Button-3>', lambda event: right_click(10))
-    pola_gry[11].bind('<Button-3>', lambda event: right_click(11))
-    pola_gry[12].bind('<Button-3>', lambda event: right_click(12))
-    pola_gry[13].bind('<Button-3>', lambda event: right_click(13))
-    pola_gry[14].bind('<Button-3>', lambda event: right_click(14))
+    for j in range(number_of_columns*number_of_rows):
+        pola_gry[j].bind('<Button-3>', lambda event: right_click(j))
+
+    # pola_gry[0].bind('<Button-3>', lambda event: right_click(0))
+    # pola_gry[1].bind('<Button-3>', lambda event: right_click(1))
+    # pola_gry[2].bind('<Button-3>', lambda event: right_click(2))
+    # pola_gry[3].bind('<Button-3>', lambda event: right_click(3))
+    # pola_gry[4].bind('<Button-3>', lambda event: right_click(4))
+    # pola_gry[5].bind('<Button-3>', lambda event: right_click(5))
+    # pola_gry[6].bind('<Button-3>', lambda event: right_click(6))
+    # pola_gry[7].bind('<Button-3>', lambda event: right_click(7))
+    # pola_gry[8].bind('<Button-3>', lambda event: right_click(8))
+    # pola_gry[9].bind('<Button-3>', lambda event: right_click(9))
+    # pola_gry[10].bind('<Button-3>', lambda event: right_click(10))
+    # pola_gry[11].bind('<Button-3>', lambda event: right_click(11))
+    # pola_gry[12].bind('<Button-3>', lambda event: right_click(12))
+    # pola_gry[13].bind('<Button-3>', lambda event: right_click(13))
+    # pola_gry[14].bind('<Button-3>', lambda event: right_click(14))
+    # pola_gry[15].bind('<Button-3>', lambda event: right_click(15))
+    # pola_gry[16].bind('<Button-3>', lambda event: right_click(16))
+    # pola_gry[17].bind('<Button-3>', lambda event: right_click(17))
+    # pola_gry[18].bind('<Button-3>', lambda event: right_click(18))
+    # pola_gry[19].bind('<Button-3>', lambda event: right_click(19))
+    # pola_gry[20].bind('<Button-3>', lambda event: right_click(20))
+    # pola_gry[21].bind('<Button-3>', lambda event: right_click(21))
+    # pola_gry[22].bind('<Button-3>', lambda event: right_click(22))
+    # pola_gry[23].bind('<Button-3>', lambda event: right_click(23))
+    # pola_gry[24].bind('<Button-3>', lambda event: right_click(24))
+    # pola_gry[25].bind('<Button-3>', lambda event: right_click(25))
+    # pola_gry[26].bind('<Button-3>', lambda event: right_click(26))
+    # pola_gry[27].bind('<Button-3>', lambda event: right_click(27))
+    # pola_gry[28].bind('<Button-3>', lambda event: right_click(28))
+    # pola_gry[29].bind('<Button-3>', lambda event: right_click(29))
+    # pola_gry[30].bind('<Button-3>', lambda event: right_click(30))
+    # pola_gry[31].bind('<Button-3>', lambda event: right_click(31))
+    # pola_gry[32].bind('<Button-3>', lambda event: right_click(32))
+    # pola_gry[33].bind('<Button-3>', lambda event: right_click(33))
+    # pola_gry[34].bind('<Button-3>', lambda event: right_click(34))
+    # pola_gry[35].bind('<Button-3>', lambda event: right_click(35))
+    # pola_gry[36].bind('<Button-3>', lambda event: right_click(36))
+    # pola_gry[37].bind('<Button-3>', lambda event: right_click(37))
+    # pola_gry[38].bind('<Button-3>', lambda event: right_click(38))
+    # pola_gry[39].bind('<Button-3>', lambda event: right_click(39))
+    # pola_gry[40].bind('<Button-3>', lambda event: right_click(40))
+    # pola_gry[41].bind('<Button-3>', lambda event: right_click(41))
+    # pola_gry[42].bind('<Button-3>', lambda event: right_click(42))
+    # pola_gry[43].bind('<Button-3>', lambda event: right_click(43))
+    # pola_gry[44].bind('<Button-3>', lambda event: right_click(44))
+    # pola_gry[45].bind('<Button-3>', lambda event: right_click(45))
+    # pola_gry[46].bind('<Button-3>', lambda event: right_click(46))
+    # pola_gry[47].bind('<Button-3>', lambda event: right_click(47))
+    # pola_gry[48].bind('<Button-3>', lambda event: right_click(48))
+    # pola_gry[49].bind('<Button-3>', lambda event: right_click(49))
+    # pola_gry[50].bind('<Button-3>', lambda event: right_click(50))
+    # pola_gry[51].bind('<Button-3>', lambda event: right_click(51))
+    # pola_gry[52].bind('<Button-3>', lambda event: right_click(52))
+    # pola_gry[53].bind('<Button-3>', lambda event: right_click(53))
+    # pola_gry[54].bind('<Button-3>', lambda event: right_click(54))
+    # pola_gry[55].bind('<Button-3>', lambda event: right_click(55))
+    # pola_gry[56].bind('<Button-3>', lambda event: right_click(56))
+    # pola_gry[57].bind('<Button-3>', lambda event: right_click(57))
+    # pola_gry[58].bind('<Button-3>', lambda event: right_click(58))
+    # pola_gry[59].bind('<Button-3>', lambda event: right_click(59))
+    # pola_gry[60].bind('<Button-3>', lambda event: right_click(60))
+    # pola_gry[61].bind('<Button-3>', lambda event: right_click(61))
+    # pola_gry[62].bind('<Button-3>', lambda event: right_click(62))
+    # pola_gry[63].bind('<Button-3>', lambda event: right_click(63))
+
 
 ##### Sprawdzanie pola - czy mina, sprawdzanie sąsiadów #####
 def check_position(i):
@@ -198,6 +264,28 @@ def reset(a):
     pola_gry[a].config(image=test, command=partial(check_position, a))
     pola_gry[a].bind('<Button-3>', lambda event: right_click(a))
 
+# def gorny_start():
+#     aktualizacjaZegara(zegar)
+#     aktualizacjaLicznikaMin(licznik_min)
+#     panel_gorny=[zegar, licznik_min]
+#     return panel_gorny
+#
+# def aktualizacjaZegara(zegar):
+#     CZAS+=1
+#     zegar["text"]="0"*(3-len(str(CZAS)))+str(CZAS)
+#     root.after(1000, aktualizacjaZegara, zegar)
+#
+# def aktualizacjaLicznikaMin(licznik_min):
+#     global LICZBAMIN
+#     for mina in range (LICZBAMIN, -1):
+#         print(mina)
+# # dałabym tutaj odniesienie do funkcji oflagowania, gdzie jest wstępna liczba flag-- poziom łatwy, średni, trudny
+#     licznik_min["text"]=LICZBAMIN
+
+CZAS=0
+LICZBAMIN=10
+LICZBASZCZUROW=LICZBAMIN
+
 root = tk.Tk()
 root.title('Saper')
 root.geometry('1200x800')
@@ -244,6 +332,23 @@ cyfra5 = ImageTk.PhotoImage(cyfra5)
 cyfra0 = Image.open("Grafiki/0.png")
 cyfra0 = cyfra0.resize((40,40))
 cyfra0 = ImageTk.PhotoImage(cyfra0)
+
+# Inicjalizacja grafik panelu górnego
+szczur = Image.open("Grafiki/szczur.png")
+szczur = szczur.resize((40,40))
+szczur = ImageTk.PhotoImage(szczur)
+
+buzka1 = Image.open("Grafiki/buzka_usmiech.png")
+buzka1 = buzka1.resize((40,40))
+buzka1 = ImageTk.PhotoImage(buzka1)
+
+buzka2 = Image.open("Grafiki/buzka_wow.png")
+buzka2 = buzka2.resize((40,40))
+buzka2 = ImageTk.PhotoImage(buzka2)
+
+buzka3 = Image.open("Grafiki/buzka_smierc.png")
+buzka3 = buzka3.resize((40,40))
+buzka3 = ImageTk.PhotoImage(buzka3)
 
 info_font = font.Font(family='Tahoma', size=20)
 button_font = font.Font(family='Tahoma', size=20)
